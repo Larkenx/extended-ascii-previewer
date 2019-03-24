f513939a7f1ab7372beb9e9561ed0c1652a6f729
Steven Myers <steven.myers@libertymutual.com>
2019-03-23 23:03:04 -0400

    working on creating a simple nodejs controller to return tileset data

--- server/index.js
+++ server/index.js
@@ -1,22 +1,44 @@
 let express = require('express')
+let os = require('os')
 let fs = require('fs')
 let path = require('path')
 let serveStatic = require('serve-static')
-let app = express()
+let getImageSize = require('image-size')
 
-const storedTilesets = null
+let storedTilesets = null
 
-async function cacheTilesets() {
-	const tilesetDirectory = path.resolve('../src/assets/tilesets/transparent')
+async function getTilesetsByDimension() {
+	const tilesetDirectory = path.resolve(__dirname, '../src/assets/tilesets/transparent')
+	const columns = 16
+	const rows = 16
 	console.log(tilesetDirectory)
 	fs.readdir(tilesetDirectory, (err, files) => {
-		files.forEach(file => {
-			console.log(file)
-		})
+		if (err) {
+			storedTilesets = null
+			console.error(err)
+		} else {
+			storedTilesets = []
+			files.forEach(file => {
+				const { width, height } = getImageSize(path.resolve(tilesetDirectory, file))
+				storedTilesets.push({
+					name: file,
+					imageWidth: width,
+					imageHeight: height,
+					spriteWidth: width / columns,
+					spriteHeight: height / rows
+				})
+			})
+		}
 	})
 }
 
+async function cacheTilesets() {
+	let tilesets = await getTilesetsByDimension()
+}
+
 cacheTilesets()
+
+// let app = express()
 
 // app.use('/', express.static(path.join(__dirname, 'dist')))
 
